1f9fa80d7efba4af6835898ff322f744792a5760
Drin K <drinkaziu@gmail.com>
2018-04-17 22:46:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ end
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.find do |cheese|
-    cheese.include?(cheese_types)
+    cheese_types.include?(cheese)
   end
 end
